d64b2e25d5badc87cca198b305ad958b2fdf35b6
LukiU <l.pangonis@gmail.com>
2019-03-30 14:37:28 +0200

    Updates

---
+++ precache-manifest.a3a9029bfc0d5fdcde092522601efa7c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ee9a2d12b569d737159f",
+    "url": "/mediaparktask/static/css/main.2eeab8d2.chunk.css"
+  },
+  {
+    "revision": "ee9a2d12b569d737159f",
+    "url": "/mediaparktask/static/js/main.e2e460d4.chunk.js"
+  },
+  {
+    "revision": "9996fa27d6bf484f82ab",
+    "url": "/mediaparktask/static/js/runtime~main.6b24f82d.js"
+  },
+  {
+    "revision": "4d6bb126340851e4b5bd",
+    "url": "/mediaparktask/static/js/2.0ee1b05b.chunk.js"
+  },
+  {
+    "revision": "07f8d09fb1358229ba96af653d4f0b9f",
+    "url": "/mediaparktask/index.html"
+  }
+];
